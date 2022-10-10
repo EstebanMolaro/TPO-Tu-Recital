748a37d65f8418f922c03dbd5aaff0a104a93fc6
Fede dC <fede_delcorro@hotmail.com>
2022-10-10 15:41:11 -0300

    subidos todos los archivos del tp

---
+++ js/index.js
@@ -0,0 +1,19 @@
+let cad =`
+        <div class="contiene-logo">
+            <a href="index.html">
+                <img class="logo" src="IMG/Logo_Tu_recital.png" alt="Logo de la página Tu Recital">
+                <img class="logo oscuro" src="IMG/Logo_Tu_recital_xaOscuro.png" alt="Logo de la página Tu Recital modo oscuro">
+            </a>
+        </div>
+        <nav class="menu">
+            <ul class="list">
+                <li><a href="index.html" style="color:rgb(237, 189, 185);">Inicio</a></li>
+                <li><a href="recitales.html">Recitales</a></li>
+                <li><a href="contacto.html">Contacto</a></li>
+                <li><a href="acerca-de.html">Acerca de</a></li>
+                <li><a href="galeria.html">Galería</a></li>
+            </ul>
+        </nav>
+`
+
+document.getElementById("head").innerHTML=cad;
